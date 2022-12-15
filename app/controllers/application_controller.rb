3af5e7e9847633731761b61660f6268efbcf6ce4
Aletha Wheeless <augustkama98@gmail.com>
2022-12-15 12:59:18 -1000

    Create Sinatra Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,40 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/games" do
+    games = Game.all
+    games.to_json(only: [:title, :genre, :platform, :price], include: :reviews)
+  end
+
+  get "/games/:id" do
+    game = Game.find(params[:id])
+    game.to_json(only: [:title, :genre, :platform, :price], include: :reviews)
+  end
+
+  get "/games/:id/reviews" do
+    game = Game.find(params[:id])
+    reviews = game.reviews.all
+    reviews.to_json
+  end
+
+  post "/games" do
+    game = Game.create(
+      title: params[:title],
+      genre: params[:genre],
+      platform: params[:platform],
+      price: params[:price]
+    )
+    game.to_json
+  end
+
+  post "/reviews" do
+    review = Review.create(
+      name: params[:name],
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id]
+    )
+    review.to_json
+  end
+
 end
